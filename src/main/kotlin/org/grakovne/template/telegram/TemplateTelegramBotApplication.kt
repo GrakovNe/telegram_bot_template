e2dc9252ddd50c8bdabf021be72b8957b10c0c2d
grakovne <grakovne@gmail.com>
2023-10-14 18:52:51 +0200

    make it template

--- src/main/kotlin/org/grakovne/template/telegram/TemplateTelegramBotApplication.kt
+++ src/main/kotlin/org/grakovne/template/telegram/TemplateTelegramBotApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KindleSideloadApplication
+class TemplateTelegramBotApplication
 
 fun main(args: Array<String>) {
-	runApplication<KindleSideloadApplication>(*args)
+	runApplication<TemplateTelegramBotApplication>(*args)
 }
